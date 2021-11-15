063ba39ade3bf90d302a0834ac23cf798ff78450
farida-techie <fairdabhairakdar@gmail.com>
2021-11-15 15:44:38 +0530

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+l
+<app/src/main/java/com/malkinfo/ecommerceapps/MainActivity.kt,6/3/630eb3f433deba0a412aa4ce3955ff8afc68052d
+Y
+)app/src/main/res/layout/activity_main.xml,7/6/76b07e40f24cce093536731477540f12e7da3cb6
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+H
+app/google-services.json,f/d/fd8d34b142d02ec743a8efefefd2c097961666e9
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+t
+Dapp/src/main/java/com/malkinfo/ecommerceapps/view/WelcomeActivity.kt,f/3/f3889c44ef52da656b72d7de872612beb1fb57f3
---
+++ app/src/main/java/com/malkinfo/ecommerceapps/view/WelcomeActivity.kt
@@ -0,0 +1,40 @@
+package com.malkinfo.ecommerceapps.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.ActionBar
+import com.malkinfo.ecommerceapps.MainActivity
+import com.malkinfo.ecommerceapps.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val actionBar :ActionBar? = supportActionBar
+        actionBar?.hide()
+
+        val imageView = findViewById<ImageView>(R.id.imageView1)
+        val ani :Animation = AnimationUtils.loadAnimation(
+            this@WelcomeActivity,R.anim.left_to_right
+        )
+        imageView.setAnimation(ani)
+        try {
+            Handler().postDelayed(
+                {
+                    startActivity(Intent(
+                        this@WelcomeActivity, MainActivity::class.java))
+                    finish()
+                },3000
+            )
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
+    }
+}
